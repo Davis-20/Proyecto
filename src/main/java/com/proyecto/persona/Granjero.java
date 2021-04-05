bf73191ae7248685d06737dd2ec5c8bccbee60fe
Davis-20 <davisinn2020@gmail.com>
2021-04-04 23:46:59 -0600

    ¨agregue_plantas_animales_y_granjro¨

--- src/main/java/com/proyecto/persona/Granjero.java
+++ src/main/java/com/proyecto/persona/Granjero.java
@@ -5,10 +5,41 @@ package com.proyecto.persona;
 
 
 public class Granjero {
-
+    private int oro;
     private int vida;
     private String nombre;
 
+    public Granjero(int oro, int vida, String nombre) {
+        this.oro = oro;
+        this.vida = vida;
+        this.nombre = nombre;
+    }
+
+    public int getOro() {
+        return oro;
+    }
+
+    public void setOro(int oro) {
+        this.oro = oro;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+   
     
 
 
--- src/main/java/com/proyecto/plantas/Planta.java
+++ src/main/java/com/proyecto/plantas/Planta.java
@@ -27,5 +27,10 @@ public abstract class Planta  {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    @Override
+    public String toString() {
+        return "Planta{" + "vida=" + vida + ", nombre=" + nombre + '}';
+    }
     
 }
