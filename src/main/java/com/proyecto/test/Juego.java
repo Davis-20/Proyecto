c012be965f8099fd1ea77470880a36a6058e992f
Davis-20 <davisinn2020@gmail.com>
2021-04-05 00:38:52 -0600

    Granjero 2 plantas y 2 animales funciona bien

--- src/main/java/com/proyecto/test/Juego.java
+++ src/main/java/com/proyecto/test/Juego.java
@@ -1,8 +1,8 @@
 
 package com.proyecto.test;
 
-import com.proyecto.animales.Animal;
-import com.proyecto.animales.Gallina;
+import com.proyecto.animales.*;
+import com.proyecto.persona.Granjero;
 
 public class Juego {
     
@@ -12,11 +12,23 @@ public class Juego {
     
     
     public void iniciarJuego(){
-        Animal gallina= new Gallina(0.5,100,"Gallina1");
-        imprime(gallina);
+        Animal gallina1= new Gallina(0.5,30,"Gallina1");
+        Animal vaca1 = new Vaca(1,100,"Vaca1");
+        Granjero granjero1 = new Granjero("Davis");
+        imprimeAnimal(gallina1);
+        imprimeAnimal(vaca1);
+        imprimeGranjero(granjero1);
     }
     
-    public static void imprime(Animal animal){
+    public static void imprimeAnimal(Animal animal){
         System.out.println(animal);
     }
+    public static void imprimePlanta(Animal animal){
+        System.out.println(animal);
+    }
+    
+    public static void imprimeGranjero(Granjero granjero){
+        System.out.println(granjero);
+    }
+    
 }
